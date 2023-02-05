763b94129997ad4d8c36eda3207060e83d61658c
mili <kamil.maksudov2019@gmail.com>
2023-02-06 00:58:18 +0100

    feat: added threlte

--- src/components/3d/Example.svelte
+++ src/components/3d/Example.svelte
@@ -1,48 +1,41 @@
 <script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
+	import { Canvas, OrbitControls, T } from '@threlte/core'
+	import { HTML, Float, Text  } from '@threlte/extras'
+	import font from '@public/fonts/helvetiker_regular.typeface.json'
+	
+
 	import { spring } from 'svelte/motion'
 	import { degToRad } from 'three/src/math/MathUtils'
 
-	const scale = spring(1)
+	const scale = spring(1);
+
+
 </script>
 
-<div>
+<div class="fixed h-screen w-screen">
 	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+		<T.PerspectiveCamera makeDefault position={[10, 0, 10]} fov={12}>
+			<OrbitControls maxPolarAngle={degToRad(80)}  enableDamping enableZoom={false} target={{ y: 0.5 }} />
 		</T.PerspectiveCamera>
 
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
+		<!-- HTML Anchor -->
+	
 
-		<!-- Cube -->
-		<T.Group scale={$scale}>
-			<T.Mesh position.y={0.5} castShadow let:ref>
-				<!-- Add interaction -->
-				<InteractiveObject
-					object={ref}
-					interactive
-					on:pointerenter={() => ($scale = 2)}
-					on:pointerleave={() => ($scale = 1)}
-				/>
+		<T.Mesh >
+			<Float speed={0}>
+				<HTML transform lookAt>
+					<a href="/blog" target="_blank" rel="noopener noreferrer">
+						Blog
+					</a>
+				</HTML>
+				
+			</Float>
 
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
-			</T.Mesh>
-		</T.Group>
+			<!-- <Text text={value} /> -->
+		</T.Mesh>
+
 
 		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
 	</Canvas>
 </div>
 
-<style>
-	div {
-		height: 100vh;
-		width: 100vw;
-	}
-</style>
